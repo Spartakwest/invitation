8bf70efd78c19680ede9a0a80371bf559cb1665e
Віктор <viktor@Admins-MacBook-Pro.local>
2023-03-26 11:38:53 +0300

    add link active

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,18 @@
 "use strict"
+// визначаємо елементи для таймера
 let days = document.querySelector('.timer--days');
 let hours = document.querySelector('.timer--hours');
 let minutes = document.querySelector('.timer--minutes');
 let secundes = document.querySelector('.timer--secundes');
+// визначаємо сьогоднішню дату та дату весілля
 let now = new Date();
 let widding = new Date(2023, 4, 13, 14);
 let diff = (widding - now) / 1000;
-
+// визначаємо дільники
 let daysdiff = 60 * 60 * 24;
 let hoursdiff = 60 * 60;
 let minutesdiff = 60;
-
+// визначаємо таймер оновлення 
 setInterval(function () {
   days.innerHTML = Math.floor(diff / daysdiff);
   hours.innerHTML = Math.floor((diff % daysdiff) / hoursdiff);
@@ -19,9 +21,5 @@ setInterval(function () {
   now = new Date();
   diff = (widding - now) / 1000;
 }, 1000);
-  // days.innerHTML = Math.floor(diff / daysdiff);
-  // hours.innerHTML = Math.floor((diff % daysdiff) / hoursdiff);
-  // minutes.innerHTML = Math.floor(((diff % daysdiff) % hoursdiff) / minutesdiff);
-  // secundes.innerHTML = Math.floor((((diff % daysdiff) % hoursdiff) % minutesdiff));
-  // now = new Date();
+
 
